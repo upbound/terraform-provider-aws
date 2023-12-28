c7ceadfdd1bd0fffaa35ac278941e3f1bfcd0331
Cem Mergenci <cmergenci@gmail.com>
2023-12-28 23:17:26 +0300

    Implement Terraform Plugin Framework provider.
    Signed-off-by: Cem Mergenci <cmergenci@gmail.com>

---
+++ xpfwprovider/xpfwprovider.go
@@ -0,0 +1,35 @@
+package xpfwprovider
+
+import (
+	"context"
+
+	fwprovider "github.com/hashicorp/terraform-plugin-framework/provider"
+	fwschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
+	internalfwprovider "github.com/hashicorp/terraform-provider-aws/internal/provider/fwprovider"
+)
+
+type AWSConfig conns.Config
+type AWSClient conns.AWSClient
+
+func GetProvider(ctx context.Context) (*fwprovider.Provider, *schema.Provider, error) {
+	p, err := provider.New(ctx)
+	fwProvider := internalfwprovider.New(p)
+	return &fwProvider, p, err
+}
+
+func GetProviderSchema(ctx context.Context) fwschema.Schema {
+	p, _ := provider.New(ctx)
+	fwProvider := internalfwprovider.New(p)
+	schemaReq := fwprovider.SchemaRequest{}
+	schemaResp := fwprovider.SchemaResponse{}
+	fwProvider.Schema(ctx, schemaReq, &schemaResp)
+	return schemaResp.Schema
+}
+
+func (ac *AWSConfig) GetClient(ctx context.Context, client *AWSClient) (*conns.AWSClient, diag.Diagnostics) {
+	return (*conns.Config)(ac).ConfigureProvider(ctx, (*conns.AWSClient)(client))
+}
