c5a30f61742d04ceee82ed5f747ffa45c46e1cd0
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2024-05-21 20:23:42 +0300

    Expose AWSClient.session via the AWSClient.Session getter function
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- internal/conns/awsclient_xp.go
+++ internal/conns/awsclient_xp.go
@@ -3,8 +3,18 @@
 
 package conns
 
-import "github.com/aws/smithy-go/middleware"
+import (
+	session_sdkv1 "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/smithy-go/middleware"
+)
 
+// AppendAPIOptions appends the specified AWS client APIOptions to
+// the client c.
 func (c *AWSClient) AppendAPIOptions(options ...func(stack *middleware.Stack) error) {
 	c.awsConfig.APIOptions = append(c.awsConfig.APIOptions, options...)
+}
+
+// Session returns the associated session with this client.
+func (c *AWSClient) Session() *session_sdkv1.Session {
+	return c.session
 }
