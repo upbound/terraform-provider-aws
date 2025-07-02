cc093a7d27cad874a6d7286b9356fe3f40c109e1
Fatih Türken <turkenf@gmail.com>
2025-07-02 20:22:10 +0300

    Update the MQ User resource to use the new framework structure: - add missing TypeName - add missing framework.ResourceValidateModel - use ListOfString type for groups field - add WithRegionModel for region support - make gen - remove depracated session method
    Signed-off-by: Fatih Türken <turkenf@gmail.com>

--- internal/service/mq/service_package_gen.go
+++ internal/service/mq/service_package_gen.go
@@ -27,8 +27,10 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*inttypes.S
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.ServicePackageFrameworkResource {
 	return []*inttypes.ServicePackageFrameworkResource{
 		{
-			Factory: newResourceUser,
-			Name:    "User",
+			Factory:  newResourceUser,
+			TypeName: "aws_mq_user",
+			Name:     "User",
+			Region:   unique.Make(inttypes.ResourceRegionDefault()),
 		},
 	}
 }
