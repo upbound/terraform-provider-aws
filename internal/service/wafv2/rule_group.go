73f55fe467b36b3e4fd820e49a0f105bb53c476d
Fatih Türken <turkenf@gmail.com>
2025-07-02 12:43:11 +0300

    resolve compile issues
    - Update AWS SDK import from v1 to v2 in mq/user.go
    - Add missing verify package import in wafv2/rule_group.go
    - Fix provider import paths and function calls in xpprovider/xpprovider.go
    
    Signed-off-by: Fatih Türken <turkenf@gmail.com>

--- internal/service/wafv2/rule_group.go
+++ internal/service/wafv2/rule_group.go
@@ -29,6 +29,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/retry"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
